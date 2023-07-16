156f5209cc681ab7e9ecccb029bcca61e4a54761
altai861 <altaizandanbileg@gmail.com>
2023-07-16 10:19:05 +0800

    Changed the fetch url to match the deployed backend

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -66,7 +66,7 @@ const Form = () => {
         formData.append('picturePath', values.picture.name)
         
         const savedUserResponse = await fetch(
-            "http://localhost:3001/auth/register",
+            "https://sociopedia-server-1pfk.onrender.com/auth/register",
             {
                 method: "POST",
                 body: formData,
@@ -82,7 +82,7 @@ const Form = () => {
 
     const login = async (values, onSubmitProps) => {
         const loggedInResponse = await fetch(
-            "http://localhost:3001/auth/login",
+            "https://sociopedia-server-1pfk.onrender.com/auth/login",
             {
                 method: "POST",
                 headers: { "Content-type": "application/json" },
